224d97b8afd00e5d9918635adae195170e590b43
Adhicitta Masran <adhicittamasran@gmail.com>
2024-12-15 11:18:48 +0800

    add laporan routes

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Report;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -16,5 +20,36 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::post('/laporan', function (Request $request) {
+    $request->validate([
+        'result' => ['string']
+    ]);
+
+    $result = $request->result;
+    echo 'result' . $result;
+
+    Report::create(['result' => $result]);
+    return redirect(route('welcome'));
+})->name('submit.result');
+
+Route::get('/laporan', function () {
+    $today = Carbon::today();
+    $startDate = $today->startOfWeek()->toDateString();
+    $endDate = $today->endOfWeek()->toDateString();
+    $totalReports = Report::select('result', DB::raw('count(*) as total'))
+    ->groupBy('result')
+    ->whereBetween('date', [$startDate, $endDate])
+    ->get();
+
+    echo $totalReports;
+})->middleware(['auth', 'verified']);
+
+Route::get('/laporan/export/excel', function () {
+    $reports = Report::get();
+
+    echo 'reports';
+    echo $reports;
+})->name('laporan.export.excel');
 
 require __DIR__.'/auth.php';
